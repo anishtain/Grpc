a27736ee2c34076f2bcbf5e23cdcf7238227814b
mohammadreza bagheri <bagheri.mohammadReza10@gmail.com>
2022-12-14 13:46:40 +0330

    add grpc client

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,20 @@
+﻿using Grpc.Net.Client;
+using GrpcServer;
+
+Console.WriteLine("Hello");
+var channel = GrpcChannel.ForAddress("https://localhost:7123/");
+var client = new Note.NoteClient(channel);
+
+var note = client.GetNote(new GetNoteRequest() { Id = 1 });
+Console.WriteLine($"{note.Title}  |   {note.Text}");
+
+var noteList = client.GetNoteList(new GetNoteListRequest());
+
+using (var call = client.GetNoteList(new GetNoteListRequest()))
+{
+    while (await call.ResponseStream.MoveNext(new CancellationToken()))
+    {
+        var n = call.ResponseStream.Current;
+        Console.WriteLine($"{n.Title}  |   {n.Text}");
+    }
+}
