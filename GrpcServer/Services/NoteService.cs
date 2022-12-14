65c62c0bd884289bc79ac3a8af7daf9d9e457b66
mohammadreza bagheri <bagheri.mohammadReza10@gmail.com>
2022-12-14 12:18:33 +0330

    Create Grpc Server

---
+++ GrpcServer/Services/NoteService.cs
@@ -0,0 +1,75 @@
+﻿using Grpc.Core;
+using GrpcServer;
+
+namespace GrpcServer.Services
+{
+    public class NoteService : Note.NoteBase
+    {
+        public override async Task<GetNoteResponse> GetNote(GetNoteRequest request, ServerCallContext context)
+        {
+            switch (request.Id)
+            {
+                case 1:
+                    return new GetNoteResponse()
+                    {
+                        Id = 1,
+                        Title = "hi",
+                        Text = "i'm first note",
+                        CreateDate = DateTime.Now.ToString()
+                    };
+                case 2:
+                    return new GetNoteResponse()
+                    {
+                        Id = 2,
+                        Title = "by",
+                        Text = "i'm second note",
+                        CreateDate = DateTime.Now.ToString()
+                    };
+                case 3:
+                    return new GetNoteResponse()
+                    {
+                        Id = 3,
+                        Title = "by",
+                        Text = "i'm thired note",
+                        CreateDate = DateTime.Now.ToString()
+                    };
+                default:
+                    return null;
+            }
+        }
+
+        public override async Task GetNoteList(GetNoteListRequest request, IServerStreamWriter<GetNoteListResponse> responseStream, ServerCallContext context)
+        {
+            var noteList = new List<GetNoteListResponse>()
+            {
+                new GetNoteListResponse()
+                    {
+                        Id = 1,
+                        Title = "hi",
+                        Text = "i'm first note",
+                        CreateDate = DateTime.Now.ToString()
+                    },
+                new GetNoteListResponse()
+                    {
+                        Id = 2,
+                        Title = "by",
+                        Text = "i'm second note",
+                        CreateDate = DateTime.Now.ToString()
+                    },
+                new GetNoteListResponse()
+                    {
+                        Id = 3,
+                        Title = "by",
+                        Text = "i'm thired note",
+                        CreateDate = DateTime.Now.ToString()
+                    }
+
+            };
+
+            foreach(var item in noteList)
+            {
+                await responseStream.WriteAsync(item);
+            }
+        }
+    }
+}
